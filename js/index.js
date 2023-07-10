9b5724e1e04ad4e47912e61a98d705be25cec732
Aleksandr Ivakhnenko <125494676+aleksandr-ivakhnenko@users.noreply.github.com>
2023-07-10 15:48:13 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,14 +1,6 @@
-const LOGO_LINK_PRELOAD_CLASSNAME = 'header__logo-link_preload';
-const LOGO_LINK_OPEN_CLASSNAME = 'header__logo-link_open';
-const OVERLAY_PRELOAD_CLASSNAME = 'header__overlay_preload';
 const BODY_FIXED_CLASSNAME = 'body_fixed';
 const BUTTON_MENU_ICON_OPEN_CLASSNAME = 'button-menu__icon_open';
 const MENU_BURGER_OPEN_CLASSNAME = 'menu-burger_open';
-
-const LOGO_LEFT_CLASSNAME = 'header__logo-left_preload';
-const LOGO_LINE_CLASSNAME = 'header__logo-line_preload';
-const LOGO_RIGHT_CLASSNAME = 'header__logo-right_preload';
-
 
 const logoLinkNode = document.querySelector('.js-logo-link');
 const overlayNode = document.querySelector('.js-overlay');
@@ -18,18 +10,10 @@ const buttonMenuIconNode = document.querySelector('.js-button-menu__icon');
 const menuBurgerNode = document.querySelector('.js-menu-burger')
 const menuBurgerContentNode = document.querySelector('.js-menu-burger__content')
 
-const logoLeftNode = document.querySelector('.js-logo-left');
-const logoLineNode = document.querySelector('.js-logo-line');
-const logoRightNode = document.querySelector('.js-logo-right');
-
-
-setTimeout(animationLogo, 100);
-setTimeout(animationPreload, 2000);
-
 buttonMenuNode.addEventListener('click', toggleBurger);
 
 menuBurgerNode.addEventListener('click', (event) => {
-    const isClickOutsideContent = !event.composedPath().includes(menuBurgerContentNode)
+    const isClickOutsideContent = !event.composedPath().includes(menuBurgerContentNode);
 
     if (isClickOutsideContent) {
         toggleBurger();
@@ -41,22 +25,6 @@ const burgerNavLinksNodes = Array.from(document.querySelectorAll('.menu-burger__
 burgerNavLinksNodes.forEach(element => {
     element.addEventListener('click', toggleBurger);
 });
-
-
-function animationPreload() {
-    logoLinkNode.classList.remove(LOGO_LINK_PRELOAD_CLASSNAME);
-    overlayNode.classList.remove(OVERLAY_PRELOAD_CLASSNAME);
-
-    setTimeout(() => {
-        bodyNode.classList.remove(BODY_FIXED_CLASSNAME);
-    }, 1000)
-};
-
-function animationLogo() {
-    logoLeftNode.classList.remove(LOGO_LEFT_CLASSNAME);
-    logoLineNode.classList.remove(LOGO_LINE_CLASSNAME);
-    logoRightNode.classList.remove(LOGO_RIGHT_CLASSNAME);
-};
 
 function toggleBurger() {
     bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
